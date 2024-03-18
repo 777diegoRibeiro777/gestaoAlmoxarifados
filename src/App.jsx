13a2da2fe51bd6315de0eb4e721611263672f44c
Diego Ribeiro <51891308+777diegoRibeiro777@users.noreply.github.com>
2024-03-18 02:12:25 -0300

    mudanças na autenticação

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./Pages/Login";
 import Produtos from "./Pages/Produtos";
-import Beanchmarking from "./Pages/Beanchmarking";
 import Request from "./Pages/Request";
 import Header from "./components/Header";
-import Configuracao from "./Pages/Configuracao";
 import { useAuthContext } from "./context/AuthContext";
 
 function App() {
-  const { isLoggedUser } = useAuthContext();
-
-  if (!isLoggedUser) {
-    return <Login />;
-  }
-
   return (
     <>
       <Header />
